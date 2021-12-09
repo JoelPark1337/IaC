1a4f4c673c4316e7b2176b6fb8a3f95873195ece
joelpark1337 <joelpark1337@gmail.com>
2021-12-08 23:40:18 -0800

    with ansible

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,4 +10,18 @@ resource "aws_instance" "ec2_server" {
     subnet_id = var.subnet_id
     vpc_security_group_ids = [var.sg_id]
     associate_public_ip_address = true
+
+    provisioner "local-exec" {
+        command = <<EOF
+            echo "[demo]" > inventory
+            echo "${aws_instance.demo.public_ip} ansible_ssh_user=ubuntu ansible_ssh_private_key_file=~/.ssh/your_private_key.pem" >> inventory
+            EOF
+    }
+
+    provisioner "local-exec" {
+        command = <<EOF
+            ANSIBLE_HOST_KEY_CHECKING=False \
+            ansible-playbook -i inventory site.yml
+            EOF
+    }
 }
